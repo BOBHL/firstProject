60d572d42da5e871785815b6d2cddc699b17322e
BOBHL <ibraheem.al.helwani@gmail.com>
2022-11-06 15:00:29 +0300

    xy

--- bin/first_project.dart
+++ bin/first_project.dart
@@ -1,5 +1,7 @@
 import 'package:first_project/first_project.dart' as first_project;
 
-void main(List<String> arguments) {
-  print('Hello world: ${first_project.calculate()}!');
+void main() {
+  //jkhjhdkjf
+
+  print('mmmm');
 }
