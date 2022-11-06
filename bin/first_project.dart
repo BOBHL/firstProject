8356402cf2db4bcb4e5442b582a60e63d61652b5
BOBHL <ibraheem.al.helwani@gmail.com>
2022-11-06 15:21:10 +0300

    First

--- bin/first_project.dart
+++ bin/first_project.dart
@@ -1,7 +1,29 @@
 import 'package:first_project/first_project.dart' as first_project;
 
 void main() {
-  //jkhjhdkjf
-  print('mmmm');
-  print('mmmm');
+  //First Code
+  var x = "IBRAHIM";
+  if (x == "IBRAHIM") {
+    String y = "Name : IBRAHIM ";
+    print(y);
+  } else {
+    String z = "Name is Fail ";
+    print(z);
+  }
+  var q = 33;
+  if (q == 33) {
+    String a = "Age : $q";
+    print(a);
+  } else {
+    String b = "Age is Folus ";
+    print(b);
+  }
+  var w = "183.5";
+  if (w == "183.5") {
+    String s = "hguit is $w ";
+    print(s);
+  } else {
+    String e = "hguit is wrog";
+    print(e);
+  }
 }
